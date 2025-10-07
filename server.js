7f0c88d1e61974d11607d90d289e1c24e461672a
Muhammad Kashif <kashif@akhtarsitsolutions.com>
2025-10-07 20:53:13 +0500

    cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
